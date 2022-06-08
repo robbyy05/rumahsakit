b0c65cab9683093c572d4ea819dfe38a376310fd
PPWSI42_robby pratama 1923240001 <84025378+robbyy05@users.noreply.github.com>
2022-06-08 08:47:52 +0700

    koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$user = "id18651426_usermotogp";
+$pass = "SI61PABLanjutan!@";
+$db = "id18651426_dbmotogp";
+
+$konek = mysqli_connect($host, $user, $pass, $db) or die("MySQL Tidak Terhubung");
+?>
